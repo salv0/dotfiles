30a84aecc471283a44ee607feb542c4c8cf1824a
Salvatore <1406472+salv0@users.noreply.github.com>
2019-06-05 13:15:43 +0200

    brew.sh: removed unnecessary tools

--- brew.sh
+++ brew.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install homebrew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+###############################################################################
+# Install command-line tools using Homebrew.																	#
+###############################################################################
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -35,9 +40,6 @@ fi;
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
 brew install grep
@@ -46,55 +48,15 @@ brew install screen
 brew install php
 brew install gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
 brew install lua
-brew install lynx
-brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
